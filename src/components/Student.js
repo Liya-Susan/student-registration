8940de186159507a2eca2c5bcc075534e5ce44e4
Liya Susan Biju <liyasusanbiju@gmail.com>
2022-07-27 20:44:33 +0530

    first commit

---
+++ src/components/Student.js
@@ -0,0 +1,35 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+import './Student';
+
+
+const Student=()=>{
+    const [name,setName] = useState('');
+    const [standard,setStandard]= useState('');
+    const [rollNo,setRollNo]=useState([]);
+
+    const onSubmitHandler = async ()=>{
+        const result = await axios.post("http://localhost:4000/student",{name,standard,rollNo});
+        if(!result) alert("something went wrong");
+    }
+
+    return(
+        <form>
+            <h2  className='pt-3 mt-5 mb-3 pb-3'>Student Registration portal</h2>
+            <div className='form-group m-2' >
+                <label>Enter the name of student</label>
+                <input type="text" className='form-control' onChange={(e)=>setName(e.target.value)}/>
+            </div>
+            <div className='form-group m-2'>
+                <label>Enter the class of student</label>
+                <input type="text" className='form-control' onChange={(e)=>setStandard(e.target.value)}/>
+            </div>
+            <div className='form-group m-2'>
+                <label>Enter the rollNo of student</label>
+                <input type="number" className='form-control' onChange={(e)=>setRollNo(e.target.value)}/>
+            </div>
+            <button type='submit ' className='btn btn-primary  m-2' onClick={onSubmitHandler}>SAVE</button>
+        </form>
+    )
+}
+export default Student;
---
+++ src/components/Students.js
@@ -0,0 +1,65 @@
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+const Students=()=>{
+    const[students,setStudents]=useState([]);
+    const [isReload,setIsReload]=useState(false)
+
+    useEffect(()=>{
+        axios.get("http://localhost:4000/students").then((res)=>setStudents(res.data.data.students))
+    },[isReload])
+
+    //const onDelete = async (rollNo)=>{
+    //    const result = await axios.delete(`http://localhost:4000/student/delete/rollNo=${rollNo}`,{name,standard,rollNo});
+    //    if(!result) alert("something went wrong!");
+    //    else setIsDeleted(true)
+    //}
+
+    const onDelete = (id) => {
+        const result = axios.delete(`http://localhost:4000/student/delete/${id}`)
+        if(!result) alert("something went wrong!");
+        else setIsReload(!isReload);
+       
+    }
+
+    const renderTable=() =>{
+        return <>
+           <h3 className='pt-3 mt-3 mb-3 pb-3'>List of Students</h3>
+           <table className='table'>
+            <thead className='table-dark'>
+                <tr>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Roll No</th>
+                    <th scope='col'>class</th>
+                    <th scope='col'></th>
+                </tr>
+            </thead>
+            <tbody>
+                {students.map(student =>(
+                    <tr key={student.rollNo}>
+                        <td>{student.name}</td>
+                        <td>{student.rollNo}</td>
+                        <td>{student.standard}</td>
+                        <td>
+                            <button type='submit' className='btn btn-danger' onClick={()=>{onDelete(student._id)}}>DELETE</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+           </table>
+           
+        </>
+    }
+
+    return(
+        //<div>
+            //{students?.length?renderTable():null}
+        //</div>
+        <>
+          {students?renderTable():null}
+        </>
+    )
+} 
+
+export default Students;
